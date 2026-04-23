4131835b32556e2b031d10e7d48f915becc6a6ed
PRIYANSHU <priyanshubhatnagar72@gmail.com>
2026-04-23 13:51:42 +0530

    Update password generator.py
    Generates password containing only letters

--- password generator.py
+++ password generator.py
@@ -1,6 +1,14 @@
 import random
-letterS=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 numbers=['1','2','3','4','5','6','7','8','9']
 symbols=['!','#','$','%','&','*','(',')','+']
 print ("Welcome to Password Generator!")
+n_letters=int(input("How many letter you want in your password?\n"))
+n_numbers=int(input("How many numbers you want in your password?\n"))
+n_symbols=int(input("How many symbols you want in your password?\n"))
+password=""
+for i in range(1,n_letters+1):
+        char=random.choice(letters)
+        password=password+char
+print(password)
