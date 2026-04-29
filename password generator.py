db015666bde6947a0b862781437fe6fff892f004
PRIYANSHU <priyanshubhatnagar72@gmail.com>
2026-04-29 23:40:21 +0530

    used random.shuffal to shuffal password_list

--- password generator.py
+++ password generator.py
@@ -7,14 +7,16 @@ print ("Welcome to Password Generator!")
 n_letters=int(input("How many letter you want in your password?\n"))
 n_numbers=int(input("How many numbers you want in your password?\n"))
 n_symbols=int(input("How many symbols you want in your password?\n"))
-password=""
+password_list=[]
 for i in range(1,n_letters+1):
         char=random.choice(letters)
-        password+=char
+        password_list+=char
 for i in range(1,n_numbers+1):
         char=random.choice(numbers)
-        password+=char
+        password_list+=char
 for i in range(1,n_symbols+1):
         char=random.choice(symbols)
-        password+=char
-print(password)
+        password_list+=char
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
